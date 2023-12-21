c33de5529ceaf3d212514433c7da6e1ee23a15eb
MaksGorobets <gorobets542@gmail.com>
2023-12-21 18:49:42 +0200

    Initial Commit

---
+++ UserManager/UserManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserManagerApp.swift
+//  UserManager
+//
+//  Created by Maks Winters on 21.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct UserManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
